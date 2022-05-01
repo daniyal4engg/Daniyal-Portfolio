c22391548ef7c95ab39d35c5ddf4e9c8d839de39
Daniyal Shaikh <daniyal4engg@gmail.com>
2022-05-02 04:09:21 +0530

    added portfolio

--- src/Pages/Projects.js
+++ src/Pages/Projects.js
@@ -38,7 +38,7 @@ function Projects() {
 
           <Col md={6} className="project-card">
             <ProjectCards
-              imgPath="https://github.com/daniyal4engg/Daniyal-Portfolio/blob/master/src/project_screenshots/dice-g301ebfc49_1920.jpg"
+              imgPath="https://github.com/daniyal4engg/Daniyal-Portfolio/blob/master/src/project_screenshots/dice-g301ebfc49_1920.jpg?raw=true"
               title="Dice Game"
               description="ToDo List App is a kind of app that generally used to maintain our day-to-day tasks or list everything that we have to do. It is helpful in planning our daily schedules. It is created by JavaScript, Html, Css languages."
               tech="Tech-Stacks"
